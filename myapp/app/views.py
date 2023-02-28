477af2200bda0ee9c783de5898ab63cff3612dea
Dmitry <stegancev.d@mail.ru>
2023-02-28 14:53:16 +0700

    Add custom urls with templates

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+class Get_all(View):
+    def get(self,request):
+        return render(request,'app/index.html')
